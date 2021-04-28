055984992aeebf3c67a101c6158af63a39d0a54b
ScidaP <patricioscida@gmail.com>
2021-04-28 16:35:56 -0300

    Implementando lista enlazada

--- ej2.c
+++ ej2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 typedef struct Tarea {
     int tareaID;
@@ -9,7 +10,16 @@ typedef struct Tarea {
     int duracion; // Entre 10 y 100
 }tarea;
 
-tarea *cargarTareas(tarea ** tareas, int cant) {
+typedef struct nodo {
+    tarea tareas;
+    nodo *sig;
+}nodo;
+
+nodo * listaVacia() {
+    return NULL;
+}
+
+void *cargarTareas(tarea ** tareas, int cant) {
     char descripciones[4][80] = {"Completar hoy", "Para el jefe", "Terminar antes de mayo", "Solicitar ayuda"};
     for (int i = 0; i < cant; i++) {
         tareas[i] = (tarea*) malloc(sizeof(tarea));
@@ -21,16 +31,48 @@ tarea *cargarTareas(tarea ** tareas, int cant) {
         //(Cliente + i)->productos->tipoProducto = (char*) malloc(sizeof(char)*270);
         //(Cliente + i)->productos->tipoProducto = (tiposProductos + rand() % (5 - 0 + 1) + 0);
     }
-    return tareas;
 }
 
-void mostrarTareas(tarea ** tareas, int cant) {
+void categorizarTareas(tarea ** tareas, int cant) {
+    nodo * start1 = listaVacia();
+    nodo * start2 = listaVacia();
     for (int j = 0; j < cant; j++) {
+        puts("-------------------------");
         printf("--- Mostrando la tarea N %d ---\n", j+1);
-        printf("-ID de la tarea: %d-\n", tareas[j]->tareaID);
-        printf("-Duracion de la tarea: %d minutos-\n", tareas[j]->duracion);
-        printf("-Descripcion de la tarea:%s-\n", tareas[j]->descripcion);
+        printf("- ID de la tarea: %d -\n", tareas[j]->tareaID);
+        printf("- Duracion de la tarea: %d minutos -\n", tareas[j]->duracion);
+        printf("- Descripcion de la tarea: %s -\n", tareas[j]->descripcion);
+        puts("|-------------------------|\n");
+        char completada;
+        printf("Considera la tarea N %d como terminada? Use S/N\n", j+1);
+        fflush(stdin);
+        scanf("%c", &completada);
+        completada = toupper(completada); // Transforma a mayúscula el carácter ingresado.
+        while (completada != 'S' && completada != 'N') {
+            printf("Caracter no reconocido: %c ...\n", completada);
+            puts("Intente nuevamente.");
+            printf("Considera la tarea N %d como terminada? S/N\n", j+1);
+            fflush(stdin);
+            scanf("%c", &completada);
+        }
+        if (completada = 'S') {
+            cargarTareasTerminadas(start1, tareas[j]);
+        } else if (completada = 'N') {
+            /* cargarTareasPendientes(tareas[j]); */
+        }
     }
+}
+
+void cargarTareasTerminadas(nodo * start, tarea * tarea) {
+    nodo * nuevoNodo = crearNodo();
+    start->sig = nuevoNodo;
+    nuevoNodo->tareas = (*tarea);
+}
+
+nodo * CrearNodo() {	
+	nodo * NuevoNodo =	(nodo*)malloc(sizeof(nodo));	
+	NuevoNodo->sig = NULL;
+	return NuevoNodo;
 }
 
 int main() {
@@ -40,7 +82,8 @@ int main() {
     scanf("%d", &cantTareas);
     tarea ** tareas = (tarea**) malloc(cantTareas*sizeof(tarea*));
     cargarTareas(tareas, cantTareas);
-    mostrarTareas(tareas, cantTareas);
+    /* mostrarTareas(tareas, cantTareas); */
+    categorizarTareas(tareas, cantTareas);
     getchar();
     getchar();
     getchar();
